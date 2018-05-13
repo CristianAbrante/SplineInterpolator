2128ff6293ebeaba951e4520fc5a4cddcbb9276c
Cristian Abrante <alu0100945850@ull.edu.es>
2018-05-13 20:21:10 +0100

    points movement implemented

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,8 +5,12 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
+
+import javax.swing.SwingUtilities;
 
 import drawable.DrawableSpline;
 import spline.Spline;
@@ -24,7 +28,7 @@ import view.VisualPanel;
  * @date 		13/05/2018
  * @version 1.0.0
  */
-public class Controller implements ActionListener {
+public class Controller implements ActionListener, MouseListener, MouseMotionListener {
   
   private final int WIDTH = 1000;
   private final int HEIGHT = 700;
@@ -41,6 +45,9 @@ public class Controller implements ActionListener {
   private SettingsPanel       settingsPanel;
   private MainFrame           frame;
   
+  private boolean             pointSelected;
+  private int                 currentSelectedPoint;
+  
   public Controller() {
     manager = new PointsManager();
     interpolator = new CubicPolynomialInterpolator();
@@ -49,6 +56,8 @@ public class Controller implements ActionListener {
     
     visualPanel = new VisualPanel(this, WIDTH, HEIGHT);
     visualPanel.setSpline(drawableSpline);
+    visualPanel.addMouseListener(this);
+    visualPanel.addMouseMotionListener(this);
     settingsPanel = new SettingsPanel(this);
     frame = new MainFrame("Spline Interpolator", settingsPanel, visualPanel);
   }
@@ -82,5 +91,92 @@ public class Controller implements ActionListener {
       }
     }
     }
+  }
+
+  /**
+   * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+   */
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    if (SwingUtilities.isRightMouseButton(e)) {
+      if (spline != null) {
+        manager.addPoint(new Point2D.Double(e.getX(), e.getY()), POINT_RADIUS);
+        spline = new Spline(interpolator, manager.getPoints());
+        drawableSpline.setSpline(spline);
+        frame.repaint();      
+      }      
+    }
+  }
+
+  /**
+   * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+   */
+  @Override
+  public void mousePressed(MouseEvent e) {
+      Point2D point = new Point2D.Double(e.getX(), e.getY());
+      currentSelectedPoint = manager.getIndexOf(point, POINT_RADIUS);
+      if (currentSelectedPoint != -1) {
+        pointSelected = true;
+      }      
+  }
+
+  /* (non-Javadoc)
+   * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+   */
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    pointSelected = false;
+  }
+
+  /* (non-Javadoc)
+   * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+   */
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  /* (non-Javadoc)
+   * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+   */
+  @Override
+  public void mouseExited(MouseEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  /* (non-Javadoc)
+   * @see java.awt.event.MouseMotionListener#mouseDragged(java.awt.event.MouseEvent)
+   */
+  @Override
+  public void mouseDragged(MouseEvent e) {
+    if (spline != null && pointSelected) {
+      if (isInBounds(e.getX(), e.getY(), currentSelectedPoint)) {
+        manager.getPoint(currentSelectedPoint).setLocation(new Point2D.Double(e.getX(), e.getY()));
+        spline.setPoints(manager.getPoints());
+        frame.repaint();        
+      }
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see java.awt.event.MouseMotionListener#mouseMoved(java.awt.event.MouseEvent)
+   */
+  @Override
+  public void mouseMoved(MouseEvent e) {
+    // TODO Auto-generated method stub
+  }
+  
+  private boolean isInBounds(int x, int y, int index) {
+    double xdouble = (double) x;
+    if (index == 0) {
+      return Double.compare(xdouble, manager.getPoint(index + 1).getX()) < 0;
+    }
+    if (index == manager.getPoints().size() - 1) {
+      return Double.compare(xdouble, manager.getPoint(index - 1).getX()) > 0;
+    }
+    return Double.compare(xdouble, manager.getPoint(index - 1).getX()) > 0
+        && Double.compare(xdouble, manager.getPoint(index + 1).getX()) < 0;
   }
 }
